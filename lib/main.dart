c1ebb91e3b03489552633688c6a5e07d5a3b52d3
jurgenizer <jurgen.geitner@gmail.com>
2022-12-14 16:49:08 +0200

    Fonts customized

--- lib/main.dart
+++ lib/main.dart
@@ -21,12 +21,22 @@ class MyApp extends StatelessWidget {
                 backgroundImage: AssetImage('images/male.png'),
               ),
               Text(
-                'Juergen Geitner',
+                'Jurgen Geitner',
                 style: TextStyle(
+                    fontFamily: 'RedHatDisplay',
                     fontSize: 30.0,
                     color: Colors.white,
                     fontWeight: FontWeight.bold),
-              )
+              ),
+              Text(
+                'MULTI-PLATFORM DEVELOPER',
+                style: TextStyle(
+                    fontFamily: 'Source Sans Pro',
+                    fontSize: 15.0,
+                    letterSpacing: 1.5,
+                    color: Colors.teal[200],
+                    fontWeight: FontWeight.bold),
+              ),
             ],
           ),
         ),
