2747a4b9a0dedf486973ed6f964429de7ed22c93
jurgenizer <jurgen.geitner@gmail.com>
2022-12-15 13:18:25 +0200

    Material Icons added

--- lib/main.dart
+++ lib/main.dart
@@ -14,6 +14,7 @@ class MyApp extends StatelessWidget {
         backgroundColor: Colors.teal,
         body: SafeArea(
           child: Column(
+            mainAxisAlignment: MainAxisAlignment.center,
             children: [
               CircleAvatar(
                 radius: 50.0,
@@ -24,18 +25,71 @@ class MyApp extends StatelessWidget {
                 'Jurgen Geitner',
                 style: TextStyle(
                     fontFamily: 'RedHatDisplay',
-                    fontSize: 30.0,
+                    fontSize: 34.0,
                     color: Colors.white,
                     fontWeight: FontWeight.bold),
               ),
               Text(
-                'MULTI-PLATFORM DEVELOPER',
+                'ALL-PLATFORM DEVELOPER',
                 style: TextStyle(
                     fontFamily: 'Source Sans Pro',
-                    fontSize: 15.0,
-                    letterSpacing: 1.5,
-                    color: Colors.teal[200],
-                    fontWeight: FontWeight.bold),
+                    fontSize: 17.0,
+                    letterSpacing: 2.0,
+                    color: Colors.teal[100],
+                    fontWeight: FontWeight.normal),
+              ),
+              Container(
+                color: Colors.white30,
+                margin: EdgeInsets.symmetric(vertical: 20.0, horizontal: 70.0),
+                padding: EdgeInsets.all(1.0),
+              ),
+              Container(
+                color: Colors.white,
+                margin: EdgeInsets.symmetric(vertical: 10.0, horizontal: 60.0),
+                padding: EdgeInsets.all(10.0),
+                child: Row(
+                  children: [
+                    Icon(
+                      Icons.phone,
+                      color: Colors.teal,
+                    ),
+                    SizedBox(
+                      width: 10.0,
+                    ),
+                    Text(
+                      '+27 082 123 9999',
+                      style: TextStyle(
+                        fontFamily: 'Source Sans Pro',
+                        fontSize: 20.0,
+                        color: Colors.teal.shade900,
+                      ),
+                    ),
+                  ],
+                ),
+              ),
+              Container(
+                color: Colors.white,
+                margin: EdgeInsets.symmetric(vertical: 10.0, horizontal: 60.0),
+                padding: EdgeInsets.all(10.0),
+                child: Row(
+                  children: [
+                    Icon(
+                      Icons.email,
+                      color: Colors.teal,
+                    ),
+                    SizedBox(
+                      width: 10.0,
+                    ),
+                    Text(
+                      'jurgen@geitner.com',
+                      style: TextStyle(
+                        fontFamily: 'Source Sans Pro',
+                        fontSize: 20.0,
+                        color: Colors.teal.shade900,
+                      ),
+                    ),
+                  ],
+                ),
               ),
             ],
           ),
